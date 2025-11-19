412d3a98576a2cbde813dd7c7ce29a0ad1a53a51
Jeff Culverhouse <jeff@graystorm.com>
2025-11-18 19:49:31 -0500

    fix: fix SemVer config

--- release.config.js
+++ release.config.js
@@ -4,7 +4,12 @@ module.exports = {
     [
       "@semantic-release/commit-analyzer",
       {
+        preset: "angular",
         releaseRules: [
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "revert", release: "patch" },
           { type: "chore", release: "patch" },
           { type: "build", release: "patch" },
           { type: "ci", release: "patch" },
@@ -12,6 +17,8 @@ module.exports = {
           { type: "refactor", release: "patch" },
           { type: "style", release: "patch" },
           { type: "test", release: "patch" },
+          { breaking: true, release: "major" },
+          { type: "*", release: "patch" }, // fallback for non-conventional commits
         ],
       },
     ],
